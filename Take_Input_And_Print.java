9c472e97d064801918fcf6aacadd11c528845266
samuel samarpana boye <97591586+samuel123457@users.noreply.github.com>
2022-09-22 10:50:53 +0530

    import java.util.Scanner; class tip {     public static void main(String args[])     {         String x;         Scanner sc=new Scanner(System.in);         x=sc.nextLine();         System.out.println(x);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class tip
+{
+    public static void main(String args[])
+    {
+        String x;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextLine();
+        System.out.println(x);
+    }
+}
