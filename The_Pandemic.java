307c4ceac2b572086b4f378d96eec44c77015297
samuel samarpana boye <97591586+samuel123457@users.noreply.github.com>
2022-10-13 15:44:34 +0530

    import java.util.*; class main{     public static void main(String args[]){         Scanner sc=new Scanner (System.in);         int n=sc.nextInt();         //int k=n-1;         //System.out.println(k);         if (n==2){             System.out.println("1");         }         else if(n==1){             System.out.println("0");         }         else{             System.out.print("2");         }     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        //int k=n-1;
+        //System.out.println(k);
+        if (n==2){
+            System.out.println("1");
+        }
+        else if(n==1){
+            System.out.println("0");
+        }
+        else{
+            System.out.print("2");
+        }
+    }
+}
